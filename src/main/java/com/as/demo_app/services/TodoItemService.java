e3c33c8db8387086112cc9984f44a4ce35338bf6
Tanais <beetee.tanais@yandex.ru>
2024-08-20 23:25:27 +0300

    feat : todo app work

--- src/main/java/com/as/demo_app/services/TodoItemService.java
+++ src/main/java/com/as/demo_app/services/TodoItemService.java
@@ -1,14 +1,8 @@
 package com.as.demo_app.services;
 
 import com.as.demo_app.dto.TodoItemDto;
-import com.as.demo_app.models.TodoItem;
-import com.as.demo_app.repositories.TodoItemRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-import java.time.Instant;
 import java.util.List;
-import java.util.Optional;
 
 public interface TodoItemService {
     TodoItemDto save(TodoItemDto todoItemDto);
